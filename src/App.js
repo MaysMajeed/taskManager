871837498fe6317727a7789966043775495abaee
HasaneenDev <hasaneennabil6477@gmail.com>
2022-04-17 08:39:15 +0300

    solve add new task

--- src/App.js
+++ src/App.js
@@ -2,23 +2,22 @@ import Tasky from "./components/DataSource";
 import AddTask from "./components/AddTask";
 import { FaPlus } from "react-icons/fa";
 import { useState } from "react";
+import { Task } from "./components/data";
 
 function App() {
   const [showForm, setShowForm] = useState(false);
-
+  const [tasks, setTasks] = useState(Task);
   const newTaskHandler = () => {
     setShowForm(!showForm);
   };
-  const AddNewTask = (task, props) => {
+  const AddNewTask = (task) => {
     const newTaskObject = {
       id: Math.floor(Math.random() * 10000),
       title: task.taskTitle,
       taskDaueDate: task.taskDueDate,
       description: task.taskDescription,
     };
-
-    console.log(newTaskObject);
-    //props.items.push({ newTaskObject });
+    setTasks(Task.unshift(newTaskObject))
   };
   return (
     <div>
@@ -27,7 +26,7 @@ function App() {
       </h1>
       {showForm && <AddTask onAdd={AddNewTask} />}
 
-      <Tasky/>
+      <Tasky tasks={tasks}/>
     </div>
   );
 }
--- src/components/DataSource.js
+++ src/components/DataSource.js
@@ -1,46 +1,8 @@
 import AllTasks from "./AllTasks";
-const Tasks = [
-  {
-    id: "1",
-    title: "Learn HTML",
-    dueDate: "today",
-    description:
-      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet, consectetur?",
-  },
-  {
-    id: "2",
-    title: "Learn CSS",
-    dueDate: "yesterday",
-    description:
-      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet, consectetur?",
-  },
-  {
-    id: "3",
-    title: "Learn JavaScript",
-    dueDate: "today",
-    description:
-      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet, consectetur?",
-  },
-  {
-    id: "4",
-    title: "Learn React",
-    dueDate: "next month",
-    description:
-      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet, consectetur?",
-  },
-  {
-    id: "5",
-    title: "Learn Node JS",
-    dueDate: "next year",
-    description:
-      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet, consectetur?",
-  },
-];
-
-function Tasky() {
+function Tasky({tasks}) {
   return (
     <div>
-      <AllTasks items={Tasks} />
+      <AllTasks items={tasks} />
     </div>
   );
 }
